5aa0b4a407748ed5f18593d41b4b9ce43b5d82e4
irapanch <117355580+irapanch@users.noreply.github.com>
2024-12-23 15:09:49 +0200

    smooth scroll

--- js/modal-constructor.js
+++ js/modal-constructor.js
@@ -1,30 +1,24 @@
-const constructorButton = document.getElementById('constructor');
-const modal = document.getElementById('modal');
-const  mainBox= document.getElementById('mainbox');
-const heroSection = document.querySelector('.hero');
+const constructorButton = document.getElementById("constructor");
+const modal = document.getElementById("modal");
+const mainBox = document.getElementById("mainbox");
+const heroSection = document.querySelector(".hero");
+const headerMain = document.querySelector(".header-main");
 
-
-const backButton = document.getElementById('back');
+const backButton = document.getElementById("back");
 
 // Показати модальне вікно
-constructorButton.addEventListener('click', () => {
-    
-    modal.style.display = 'block';
-    
-    mainBox.style.display = 'none';
-    heroSection.scrollIntoView({ behavior: 'smooth', block: 'start' });
-    
-    
-});
+constructorButton.addEventListener("click", () => {
+  modal.style.display = "block";
 
+  mainBox.style.display = "none";
+  heroSection.scrollIntoView({ behavior: "smooth", block: "start" });
+});
 
 // Закрити модальне вікно
-backButton.addEventListener('click', () => {
-    modal.style.display = 'none';
-   
-    mainBox.style.display = 'block';
-    
+backButton.addEventListener("click", () => {
+  modal.style.display = "none";
+  mainBox.style.display = "block";
 
+  // Прокрутка до header-main з плавною анімацією
+  headerMain.scrollIntoView({ behavior: "smooth", block: "start" });
 });
-
-
