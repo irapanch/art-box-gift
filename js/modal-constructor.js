6d7853fbfc05e41a6ddd204912f91155477230a9
irapanch <117355580+irapanch@users.noreply.github.com>
2024-12-24 12:28:01 +0200

    fix section add video

--- js/modal-constructor.js
+++ js/modal-constructor.js
@@ -1,24 +1,25 @@
-const constructorButton = document.getElementById("constructor");
+const orderButtons = document.querySelectorAll(".btn-order-js");
 const modal = document.getElementById("modal");
 const mainBox = document.getElementById("mainbox");
 const heroSection = document.querySelector(".hero");
 const headerMain = document.querySelector(".header-main");
 
 const backButton = document.getElementById("back");
-
-// Показати модальне вікно
-constructorButton.addEventListener("click", () => {
-  modal.style.display = "block";
-
-  mainBox.style.display = "none";
-  heroSection.scrollIntoView({ behavior: "smooth", block: "start" });
-});
-
-// Закрити модальне вікно
-backButton.addEventListener("click", () => {
-  modal.style.display = "none";
-  mainBox.style.display = "block";
-
-  // Прокрутка до header-main з плавною анімацією
-  headerMain.scrollIntoView({ behavior: "smooth", block: "start" });
-});
+// Показать модальное окно при клике на любую кнопку с классом btn-order
+orderButtons.forEach(button => {
+    button.addEventListener("click", () => {
+      modal.style.display = "block";
+  
+      mainBox.style.display = "none";
+      heroSection.scrollIntoView({ behavior: "smooth", block: "start" });
+    });
+  });
+  
+  // Закрыть модальное окно
+  backButton.addEventListener("click", () => {
+    modal.style.display = "none";
+    mainBox.style.display = "block";
+  
+    // Прокрутка до header-main с плавной анимацией
+    headerMain.scrollIntoView({ behavior: "smooth", block: "start" });
+  });
