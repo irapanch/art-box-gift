6661b199ea7f6f16da7be1e91286f4609e4d7081
irapanch <117355580+irapanch@users.noreply.github.com>
2024-12-26 09:05:05 +0200

    fix&optimization imgs

--- js/modal-constructor.js
+++ js/modal-constructor.js
@@ -1,28 +1,4 @@
-// const orderButtons = document.querySelectorAll(".btn-order-js");
-// const modal = document.getElementById("modal");
-// const mainBox = document.getElementById("mainbox");
-// const heroSection = document.querySelector(".hero");
-// const headerMain = document.querySelector(".header-main");
 
-// const backButton = document.getElementById("back");
-// // Показать модальное окно при клике на любую кнопку с классом btn-order
-// orderButtons.forEach(button => {
-//     button.addEventListener("click", () => {
-//       modal.style.display = "block";
-  
-//       mainBox.style.display = "none";
-//       heroSection.scrollIntoView({ behavior: "smooth", block: "start" });
-//     });
-//   });
-  
-//   // Закрыть модальное окно
-//   backButton.addEventListener("click", () => {
-//     modal.style.display = "none";
-//     mainBox.style.display = "block";
-  
-//     // Прокрутка до header-main с плавной анимацией
-//     headerMain.scrollIntoView({ behavior: "smooth", block: "start" });
-//   });
 
 const orderButtons = document.querySelectorAll(".btn-order-js");
 const modal = document.getElementById("modal");
